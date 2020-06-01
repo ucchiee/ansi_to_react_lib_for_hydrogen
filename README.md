b47f273aec29c493c684d49e79c7e4c344913c85
Yusuke Uchida <fantomyuu0623@gmail.com>
2020-06-02 02:59:16 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ansi_to_react_lib_for_hydrogen
-ansi_to_react_lib_for_hydrogen
+solution to issue #1944, #1945, #1946
+
+Copy `lib` to `~/.atom/packages/Hydrogen/node_modules/ansi_to_react/lib`
+
+Enjoy your Hydrogen!
---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+declare interface Props {
+    children?: string;
+    linkify?: boolean;
+    className?: string;
+    useClasses?: boolean;
+}
+export default function Ansi(props: Props): JSX.Element;
+export {};
